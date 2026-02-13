c3e5ea0308b4fbd8d732e3199ce723d89486a44d
Захар Роговский <rogovskiy@echo-company.ru>
2026-02-14 00:28:15 +0600

    Обновить шаблон хлебных крошек: улучшить структуру кода и добавить вывод элементов списка

--- local/templates/main/components/bitrix/breadcrumb/main/template.php
+++ local/templates/main/components/bitrix/breadcrumb/main/template.php
@@ -1,26 +1,41 @@
 <?php
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
-    die();
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+global $APPLICATION;
+
+if(empty($arResult))
+	return "";
+
+$strReturn = '';
+
+$strReturn .= '<div class="breadcrumbs"><ul itemprop="http://schema.org/breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
+
+$itemSize = count($arResult);
+for($index = 0; $index < $itemSize; $index++)
+{
+	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+	{
+		$strReturn .= '
+			<li id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" class="link">
+				<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" itemprop="item">'.$title.'</a>
+				<meta itemprop="position" content="'.($index + 1).'" />
+			</li>';
+	}
+	else
+	{
+		if (!empty($title )) {
+		$strReturn .= '
+				<li>'.$title.'</li>';
+		}
+      }
 }
 
-if (empty($arResult)) {
-    return;
-}
-?>
-<ul>
-<?php $itemsCount = count($arResult); ?>
-<?php $position = 0; ?>
-<?php foreach ($arResult as $arItem): ?>
-    <?php
-    $position++;
-    $isLast = ($position === $itemsCount);
-    ?>
-    <li>
-        <?php if (!$isLast): ?>
-            <a href="<?= htmlspecialcharsbx($arItem['LINK']) ?>"><?= htmlspecialcharsbx($arItem['TITLE']) ?></a>
-        <?php else: ?>
-            <?= htmlspecialcharsbx($arItem['TITLE']) ?>
-        <?php endif; ?>
-    </li>
-<?php endforeach; ?>
-</ul>
+$strReturn .= '</ul></div>';
+
+return $strReturn;
