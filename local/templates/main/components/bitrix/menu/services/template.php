24ad687d4fe5001f5d81bca13c816e0f375345aa
Захар Роговский <rogovskiy@echo-company.ru>
2026-02-14 00:44:39 +0600

    Меню

--- local/templates/main/components/bitrix/menu/services/template.php
+++ local/templates/main/components/bitrix/menu/services/template.php
@@ -4,70 +4,17 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 }
 
 if (empty($arResult)) {
-    $arResult = [
-        [
-            'TEXT' => 'аренда<br>строительных лесов',
-            'LINK' => '/services/rent-scaffolding/',
-            'SELECTED' => false,
-            'PERMISSION' => 'R',
-            'PARAMS' => ['HTML' => 'Y'],
-        ],
-        [
-            'TEXT' => 'продажа<br>строительных лесов',
-            'LINK' => '/services/sale-scaffolding/',
-            'SELECTED' => false,
-            'PERMISSION' => 'R',
-            'PARAMS' => ['HTML' => 'Y'],
-        ],
-        [
-            'TEXT' => 'леса<br>строительные б/у',
-            'LINK' => '/services/scaffolding-used/',
-            'SELECTED' => false,
-            'PERMISSION' => 'R',
-            'PARAMS' => ['HTML' => 'Y'],
-        ],
-        [
-            'TEXT' => 'аренда вышки туры',
-            'LINK' => '/services/tower-rental/',
-            'SELECTED' => false,
-            'PERMISSION' => 'R',
-        ],
-    ];
+    return;
 }
-
-global $APPLICATION;
-$currentPage = is_object($APPLICATION) ? $APPLICATION->GetCurPage(false) : '';
-$currentPageNormalized = rtrim(str_replace('/index.php', '/', $currentPage), '/') . '/';
 ?>
 <ul>
 <?php foreach ($arResult as $arItem): ?>
-    <?php
-    $itemLink = !empty($arItem['LINK']) ? $arItem['LINK'] : '#';
-    $itemLinkNormalized = rtrim(str_replace('/index.php', '/', $itemLink), '/') . '/';
-    $isSelected = !empty($arItem['SELECTED']) || ($itemLink !== '#' && $itemLinkNormalized === $currentPageNormalized);
-    $className = $isSelected ? ' class="active"' : '';
-    ?>
+    <?php $className = $arItem['SELECTED'] ? ' class="active"' : ''; ?>
     <li<?= $className ?>>
         <?php if ($arItem['PERMISSION'] > 'D'): ?>
-            <a href="<?= htmlspecialcharsbx($itemLink) ?>">
-                <?php
-                if (!empty($arItem['PARAMS']['HTML'])) {
-                    echo strip_tags($arItem['TEXT'], '<br>');
-                } else {
-                    echo htmlspecialcharsbx($arItem['TEXT']);
-                }
-                ?>
-            </a>
+            <a href="<?= htmlspecialcharsbx($arItem['LINK']) ?>"><?= htmlspecialcharsbx($arItem['TEXT']) ?></a>
         <?php else: ?>
-            <a href="#">
-                <?php
-                if (!empty($arItem['PARAMS']['HTML'])) {
-                    echo strip_tags($arItem['TEXT'], '<br>');
-                } else {
-                    echo htmlspecialcharsbx($arItem['TEXT']);
-                }
-                ?>
-            </a>
+            <a href="#"><?= htmlspecialcharsbx($arItem['TEXT']) ?></a>
         <?php endif; ?>
     </li>
 <?php endforeach; ?>
